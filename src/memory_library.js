2af662692df3f52905a341fc068161653305cded
Vismaya P S <vismayaps@Vismayas-MacBook-Pro.local>
2026-02-10 10:00:52 +0530

    Implements libraryRegistry
    * creates class libraryRegistry
    * adds addBook method in libraryRegistry
    * adds updateQuantity in libraryRegistry

--- src/memory_library.js
+++ src/memory_library.js
@@ -1,6 +1,29 @@
-export const createBookCatalog = () => [];
+export class LibraryRegistry {
+  constructor() {
+    this.bookCatalog = [];
+    this.bookId = 0;
+  }
+  #doesBookExists = (bookDetails) =>
+    this.bookCatalog.some((book) => book.name === bookDetails.name);
 
-export const addBook = (bookCatalog, bookDetails) => {
-  bookCatalog.push(bookDetails);
-  return { success: true };
-};
+  addBook(bookDetails) {
+    if (this.#doesBookExists(bookDetails)) {
+      return { success: false, errorCode: 201 };
+    }
+
+    this.bookCatalog.push(bookDetails);
+    return { success: true };
+  }
+
+  updateQuantity(id, value) {
+    const bookDetails = this.bookCatalog
+      .find((bookDetail) => bookDetail.id === id);
+
+    if (!bookDetails) {
+      return { success: false, errorCode: 301 };
+    }
+
+    bookDetails.quantity += value;
+    return { success: true };
+  }
+}
--- test/memory_library_test.js
+++ test/memory_library_test.js
@@ -1,24 +1,53 @@
 import { assertEquals } from "@std/assert";
 import { beforeEach, describe, it } from "@std/testing";
-import { addBook, createBookCatalog } from "../src/memory_library.js";
+import { LibraryRegistry } from "../src/memory_library.js";
 
-describe("CREATES BOOK CATALOG : ", () => {
-  it("=> should creates a table bookCatalog", () => {
-    assertEquals(createBookCatalog(), []);
+describe("ADDS BOOKS : ", () => {
+  let libraryRegistry;
+  beforeEach(() => {
+    libraryRegistry = new LibraryRegistry();
+  });
+  it("=> should add a new book to bookCatalog: 'Pinocchio'", () => {
+    const bookDetails = {
+      id: 1,
+      name: "Pinocchio",
+      catrgory: "story",
+      price: 399,
+      quantity: 10,
+    };
+    assertEquals(libraryRegistry.addBook(bookDetails), { success: true });
+  });
+  it("=> shouldn't add a book to bookCatalog: book already exists", () => {
+    const bookDetails = {
+      id: 1,
+      name: "Pinocchio",
+      catrgory: "story",
+      price: 399,
+      quantity: 10,
+    };
+    libraryRegistry.addBook(bookDetails);
+    assertEquals(libraryRegistry.addBook(bookDetails), {
+      success: false,
+      errorCode: 201,
+    });
   });
 });
 
-describe("ADDS BOOKS : ", () => {
-  let bookCatalog;
+describe("UPDATE QUANTITY", () => {
+  let libraryRegistry;
   beforeEach(() => {
-    bookCatalog = createBookCatalog();
+    libraryRegistry = new LibraryRegistry();
   });
-  it("=> should add a book to bookCatalog: 'Diary Of Wimpy Kid'", () => {
+  it("=> should update quantity: pinocchio", () => {
     const bookDetails = {
-      name: "Diary Of Wimpy Kid",
-      price: 299,
+      id: 1,
+      name: "Pinocchio",
+      catrgory: "story",
+      price: 399,
       quantity: 10,
     };
-    assertEquals(addBook(bookCatalog, bookDetails), { success: true });
+
+    libraryRegistry.addBook(bookDetails);
+    assertEquals(libraryRegistry.updateQuantity(1, 1), { success: true });
   });
 });
